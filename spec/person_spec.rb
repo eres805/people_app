35e3a9ab5d6d7d58a7f964ab8cb825d1bab93ecb
eres805 <eres805@gmail.com>
2014-04-06 19:45:14 -0400

    birthday test working

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Person do
-	subject(:person) {Person.create(first_name: "John",last_name: "Smith")}
+	subject(:person) {Person.create(first_name: "John",last_name: "Smith", birthday: "1990-01-01")}
 	
 	describe :first_name do
 		it "has a first name" do
@@ -12,6 +12,12 @@ describe Person do
 	describe :last_name do
 		it "has a last name" do
 			expect(person.last_name).to eql("Smith")
+		end
+	end
+
+	describe :birthday do
+		it "has a birthday" do 
+			expect(person.birthday).to_not be_nil
 		end
 	end
 
